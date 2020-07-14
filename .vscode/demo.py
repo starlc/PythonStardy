54807b5ec59c68b0e036690629c68c16b5c1e842
liuc <liucheng@authine.com>
2020-07-14 17:38:01 +0800

    添加练习代码

---
+++ .vscode/demo.py
@@ -0,0 +1,14 @@
+msg = 'hello zhangsan,\
+nice to meet you'
+
+print(msg)
+
+#自然字符串
+msg2 = r"Newlines are indicated by \n"
+print(msg2)
+
+msg3 =u"This is a Unicode string."
+print(msg3)
+
+msg4 = 'What\'s' ' your name?'
+print(msg4)
---
+++ .vscode/var.py
@@ -0,0 +1,5 @@
+# FileName : var.py
+i=5
+print(i) 
+i=i+1
+print(i)
