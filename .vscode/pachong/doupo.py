3232cea97fa2e4bb8c16ebfa912993af7bb1383b
liuc <liucheng@authine.com>
2020-07-15 16:40:43 +0800

    第一个爬虫，拔了俩页小说

--- .vscode/pachong/doupo.py
+++ .vscode/pachong/doupo.py
@@ -1,18 +1,18 @@
 import requests
 import re
 import time
-tmp_dir = r'D:\PythonWork\doupo.txt'
+tmp_dir = r'D:\PythonWork\doupo.html'
 f = open(tmp_dir,'a+')
 def get_info(url):
     res = requests.get(url)
     if res.status_code == 200:
-        contents = re.findall('<p>(.*?)</p>',res.content.decode('utf-8'),re.S)
+        contents = re.findall('<div id="content" deep="3">(.*?)</div>',res.content.decode('utf-8'),re.S)
         for content in contents:
             f.write(content+'\n')
         else:
             pass
 if __name__ =='__main__':
-    urls = ['https://www.5atxt.com/24_24752/{}.html'.format(str(i)) for i in range(20066785,21488000)]
+    urls = ['https://www.5atxt.com/24_24752/{}.html'.format(str(i)) for i in range(20066785,20066787)]
     for url in urls:
         get_info(url)
         time.sleep(1)
