ecfb21ac23570e9587435d194ed817f289fd257c
starlc <starlc_2588@163.com>
2020-11-23 23:00:27 +0800

    修正数据

--- .vscode/export/export_org.py
+++ .vscode/export/export_org.py
@@ -1,7 +1,7 @@
 import xlrd
 import xlsxwriter
 # 文件名以及路径，前面加一个r防止生成不必要的转义。
-filename = r'D:\Python\workspace\组织导入模板.xlsx'
+filename = r'.vscode\export\组织导入模板.xlsx'
 data = xlrd.open_workbook(filename)
 # 获取第1个sheet页
 table = data.sheets()[0]
@@ -28,7 +28,7 @@ data1 = convertRowToTuple(1)
 # print(header)
 # print(data1)
 # 即将写入数据的文件名
-filename2 = r'D:\Python\workspace\组织导入模板--5W.xlsx'
+filename2 = r'.vscode\export\组织导入模板--5W.xlsx'
 # 如果数据量非常大，可以启用constant_memory，这是一种顺序写入模式，得到一行数据就立刻写入一行，而不会把所有的数据都保持在内存中。
 # 如果不启用此模式，当数据量巨大时，程序很大概率地卡死
 workbook = xlsxwriter.Workbook(filename2, {'constant_memory': True})
@@ -37,57 +37,78 @@ worksheet = workbook.add_worksheet()
 # startNum表示从第几行开始写，这里的数字是从1开始，因为后面要和字母组合对应在excel中，如A1代表第1行第A列
 startNum = 1
 # 初始值，后面的数字在它们的基础上依次增加
-startValues = ['0000001', '0000001',
-               '0000001', '0000001', '1000001', '1000001']
+startValues = [
+    '0000001', '0000001', '0000001', '0000001', '1000001', '1000001'
+]
 # 初始值对应的列
 col = ['A', 'B', 'C', 'D']
-indexs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-          'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q']
+indexs = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q'
+]
 # 先将表头写入文件
-worksheet.write_row('A'+str(startNum), header)
+worksheet.write_row('A' + str(startNum), header)
 
 
 # 补齐N位
 def getRealVal(prefix, content, length):
     if length != 0:
         content = str(content)
-        content = '0'*(length-len(content))+content
-        content = prefix+content
+        content = '0' * (length - len(content)) + content
+        content = prefix + content
     return content
+
 
 start_index = 0
 mydate = list(data1)
+one_org = []
+two_org = []
+three_org = []
+four_org = []
+five_org = []
+six_org = []
+
+
+def roundVal(i, sub, div, mod):
+    return int((i - sub) / div) % mod
+
+
 # 正式开始写入数据
 for i in range(50000):
 
-    mydate[0] = getRealVal("", i+1, 0)
+    if i < 100:
+        print(int(i / 2) % 100)
+    mydate[0] = getRealVal("", i + 1, 0)
     # 小于5000为二级组织
-    if(i < 5000 ):
-        mydate[1] = getRealVal("二级组织", i+1, 5)
-        mydate[2] = getRealVal("zz_BA2f_", i+1, 5)
-        
-
+    if (i < 5000):
+        mydate[1] = getRealVal("二级组织", i + 1, 5)
+        mydate[2] = getRealVal("zz_BA2f_", i + 1, 5)
+        two_org.append(mydate[2])
     elif i < 10000:
-        mydate[1] = getRealVal("三级组织", i-4999, 5)
-        mydate[2] = getRealVal("zz_BA3f_", i-4999, 5)
-        mydate[3] = getRealVal("zz_BA2f_", (i-4999)%5000, 5)
+        mydate[1] = getRealVal("三级组织", i - 4999, 5)
+        mydate[2] = getRealVal("zz_BA3f_", i - 4999, 5)
+        mydate[3] = two_org[roundVal(i, 5000, 1, 5000)]
+        three_org.append(mydate[2])
     elif i < 20000:
-        mydate[1] = getRealVal("四级组织", i-9999, 5)
-        mydate[2] = getRealVal("zz_BA4f_", i-9999, 5)
-        mydate[3] = getRealVal("zz_BA3f_", (i-9999)%10000, 5)
+        mydate[1] = getRealVal("四级组织", i - 9999, 5)
+        mydate[2] = getRealVal("zz_BA4f_", i - 9999, 5)
+        mydate[3] = three_org[roundVal(i, 10000, 2, 5000)]
+        four_org.append(mydate[2])
     elif i < 30000:
-        mydate[1] = getRealVal("五级组织", i-19999, 5)
-        mydate[2] = getRealVal("zz_BA5f_", i-19999, 5)
-        mydate[3] = getRealVal("zz_BA4f_", (i-19999)%10000, 5)
+        mydate[1] = getRealVal("五级组织", i - 19999, 5)
+        mydate[2] = getRealVal("zz_BA5f_", i - 19999, 5)
+        mydate[3] = four_org[roundVal(i, 20000, 1, 10000)]
+        five_org.append(mydate[2])
     elif i < 40000:
-        mydate[1] = getRealVal("六级组织", i-29999, 5)
-        mydate[2] = getRealVal("zz_BA6f_", i-29999, 5)
-        mydate[3] = getRealVal("zz_BA5f_", (i-29999)%10000, 5)
+        mydate[1] = getRealVal("六级组织", i - 29999, 5)
+        mydate[2] = getRealVal("zz_BA6f_", i - 29999, 5)
+        mydate[3] = five_org[roundVal(i, 30000, 1, 10000)]
+        six_org.append(mydate[2])
     else:
-        mydate[1] = getRealVal("七级组织", i-39999, 5)
-        mydate[2] = getRealVal("zz_BA7f_", i-39999, 5)
-        mydate[3] = getRealVal("zz_BA6f_", (i-39999)%20000, 5)
+        mydate[1] = getRealVal("七级组织", i - 39999, 5)
+        mydate[2] = getRealVal("zz_BA7f_", i - 39999, 5)
+        mydate[3] = six_org[roundVal(i, 40000, 1, 10000)]
 
-    worksheet.write_row('A'+str(startNum+i+1), tuple(mydate))
-        # 写完之后关闭workbook，否则会报错
+    worksheet.write_row('A' + str(startNum + i + 1), tuple(mydate))
+    # 写完之后关闭workbook，否则会报错
 workbook.close()
